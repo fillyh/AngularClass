098c4820f0f3450760c04a6a670787f7f1b0889b
felipe hernandez <felipehernandez@felipes-MacBook-Pro.local>
2018-07-07 14:34:27 -0700

    day two

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() {}
+  greeting = 'Welcome to my Angular App';
+  introduction = ' A very large Angular application ... I guees';
+  text = '';
+  list = ['one', 'two', 'three', 'four', 'five']
+  ngOnInit() {
+  }
+
+}
